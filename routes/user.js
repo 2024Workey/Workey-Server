ed3170e244fef58d2e11dae5dca2f1b4dc0d4020
haeunNoh06 <okhagrace06@naver.com>
2024-01-15 13:32:53 +1100

    :sparkles: [add] session bcrypt 추가

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const { User } = require('../models');// index는 파일 이름 생략 가능 
 const { Op } = require("sequelize");
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy;
 
 const router = express.Router();
+
+const salt = 12;// 값이 높을수록 암호화 연산 증가 
 
 // endpoint
 // 시퀄라이즈가 제공하는 건 모두 비동기함수
 router.post('/join', async (req, res) => {
     try {
         const user = await User.create(req.body);
-        return res.status(201).json({"message" : req.body.email+" join success"});
+        return res.status(201).json({ "message": req.body.email + " join success" });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ success: false, message: "Error creating user" });
@@ -17,21 +21,41 @@ router.post('/join', async (req, res) => {
 });
 
 // 로그인
-router.get('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ // 이메일 검사
-            where: { // where : DB에서 조건을 건다.
-                email: email,
-                password: password,
+router.get('/login', async (req, res, passport) => {
+    const { email, password } = req.body;
+    bcrypt.hash(PW, salt, (err, encryptedPW) => {
+
+    })
+
+    // hashSync 동기
+    const hash = bcrypt.hashSync(PW, salt);
+
+    // async/await 사용
+    // const hash = await bcrypt.hash(PW, salt)
+
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+    }, async (email, password, done) => {
+        try {
+            const exUser = await User.findOne({ where: { email } });
+            if (exUser) {
+                const result = await bcrypt.compare(password, exUser.password);
+                if (result) {
+                    done(null, exUser);
+                } else {
+                    done(null, false, { message: '비밀번호가 일치하지 않습니다.' });
+                }
+            } else {
+                done(null, false, { message: '가입되지 않은 회원입니다.' });
             }
-        });
-        if (user) {
-            return res.status(200).json({"message": "login success"});
+        } catch (err) {
+            console.error(err);
+            done(err);
         }
-        
-    } catch (error) {
-        return res.status(500).json({ "error": "login fail"})
+    }));
+    if (user) {
+        return res.status(200).json({ "message": "login success" });
     }
 });
 
