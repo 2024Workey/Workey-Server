098c2e0cb384ca2fa13789a22719b363d9eed8b9
ezi-s-u <ljsgoo0617@gmail.com>
2024-01-15 13:36:04 +1100

    :sparkles: [Feat] 회사 이름 추가 API

--- routes/company.js
+++ routes/company.js
@@ -1,8 +1,29 @@
 const express = require('express');
-const { User } = require('../models');// index는 파일 이름 생략 가능 
+const { Company } = require('../models');// index는 파일 이름 생략 가능 
 
 const router = express.Router();
 
+// 자기 회사 입력하기
+router.post('/', async (req,res) => {
+  try {
+    const company = await Company.create(req.body);
+    return res.status(201).json({ "id" : company.dataValues.id, "message": "회사 추가에 성공하였습니다." })
+  } catch(err) {
+    console.error(err);
+    return res.status(500).json({
+      "message": "회사 추가에 실패하였습니다."
+    })
+  }
+})
+
+// 회사 list (ranking)
+router.get('/', (req, res) => {
+  try {
+
+  } catch(err) {
+
+  }
+})
 
 
 module.exports = router;
