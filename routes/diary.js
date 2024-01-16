47a1c017343c8b26953dae2729cd81fbc40f209b
ezi-s-u <ljsgoo0617@gmail.com>
2024-01-16 12:15:51 +1100

    :sparkles: [feat] 글 작성 API

--- routes/diary.js
+++ routes/diary.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const { User } = require('../models');// index는 파일 이름 생략 가능 
+const { Diary } = require('../models');// index는 파일 이름 생략 가능 
 
 const router = express.Router();
 
-
+router.post('/:user_id/:ques_id', async (req, res) => {
+  try {
+    const diary = await Diary.create({
+      userId : 1,
+      quesId : req.params.ques_id,
+      ...req.body
+    });
+    //console.log(diary);
+    return res.status(201).json( {"message" : "셀프 체크 테스트 결과 저장에 성공했습니다."} )
+  } catch(err) {
+    console.error(err)
+    return res.status(500).json( { "message": "글 작성에 실패하였습니다." } );
+  }
+})
 
 module.exports = router;
