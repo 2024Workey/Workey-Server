0a0489c13a1b57c6ffd6e37e220effec555ff96c
haeunNoh06 <okhagrace06@naver.com>
2024-01-24 22:17:41 +1100

    :sparkles: [feat] 질문 하나만 가져오기 api 구현

--- routes/question.js
+++ routes/question.js
@@ -7,15 +7,16 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     // const todayDate = now;
+    // const questionId = req.params.questionId;
     const date = moment().format("DD");
     try {
         const question = await Question.findOne({
-            attributes: ['id', 'question'],
             where: { id: date },
         })
+        console.log(question);
         if ( question ) {
             return res.status(200).json({
-                "success": question
+                "data": question
             });
         } else {
             return res.status(404).json({
@@ -31,5 +32,47 @@ router.get("/", async (req, res) => {
         })
     }
 })
+
+router.post('/', async (req, res) => {
+    try {
+        const {  } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = await User.create({
+            ...req.body,
+            password: hashedPassword,
+        });
+        console.log("유저탄생 " + newUser);
+
+        // req.session.save((err) => {
+        //     if (err) {
+        //         console.log("에러임 " + err)
+        //     }
+        // })
+
+        // req.session.user = newUser;
+        // console.log("세션확인 : "+req.session.user);
+
+        return res.status(201).json({ "message": req.body.email + " join success" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: "Error creating user" });
+    }
+});
+
+router.get('/:quesId', async (req, res) => {
+    try {
+      const question = await Question.findOne({
+        where: {
+          id : req.params.quesId 
+        }
+      })
+      return res.status(200).json( question.dataValues.question )
+    } catch(err) {
+      console.error(err)
+      return res.status(500).json( { "message": "글 단독 불러오기에 실패하였습니다." } );
+    }
+  })
 
 module.exports = router;
